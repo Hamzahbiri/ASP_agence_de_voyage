48348216fa0dea62e14cfe54d9bca8205344ba91
hamzahbiri <hbiri.hamza@iit.ens.tn>
2023-11-05 15:30:47 +0100

    Users views

--- Agence_de_Voyages/Models/UsersModel.cs
+++ Agence_de_Voyages/Models/UsersModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Numerics;
+using System;
 
 namespace Agence_de_Voyages.Models
 {
@@ -12,27 +13,27 @@ namespace Agence_de_Voyages.Models
 		[MinLength(3, ErrorMessage = "Name contains at least 3 letters")]
 		[MaxLength(20, ErrorMessage="Name is too long")]
 		[RegularExpression("^[a-zA-Z ]*$", ErrorMessage = "Name should not contain special characters")]
-		public string Name { get; set; }
+		public String? Name { get; set; }
 
 		[Required(ErrorMessage = "Age field is Required")]
 		[Range(18, 80, ErrorMessage = "Age must be between 18 and 80")]
 
 		public int Age { get; set; }
 		[Required(ErrorMessage = "Gender field is Required")]
-		public string Gender { get; set; }
+		public String Gender { get; set; }
 
 		[Required(ErrorMessage = "Email field is Required")]
 		[EmailAddress(ErrorMessage = "Invalid Email Address")]
-		public string Email { get; set; }
+		public String Email { get; set; }
 
 		[Required(ErrorMessage = "Phone field is Required")]
-		public string Phone { get; set; }
+		public String Phone { get; set; }
 
 		[Required(ErrorMessage = "Type field is Required")]
-		public string Type { get; set; }
+		public String Type { get; set; }
 
 		[Required(ErrorMessage = "Password field is Required")]
 		[MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
-		public string Password { get; set; }
+		public String Password { get; set; }
 	}
 }
