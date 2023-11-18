5da2d7905b8fe874440d7495250e8c886fa6943a
chedifeki <chedifeki@gmail.com>
2023-11-18 13:10:53 +0100

    Conditional Venues and better display of venue type

---
+++ Agence_de_Voyages/Migrations/20231118103531_Venues.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Agence_de_Voyages.Migrations
+{
+    /// <inheritdoc />
+    public partial class Venues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "PricePerNight",
+                table: "Venues",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "EntryFee",
+                table: "Venues",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PricePerNight",
+                table: "Venues",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EntryFee",
+                table: "Venues",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+        }
+    }
+}
